610de73f9f384a5fbbbf23c3cdf69cd7cd38a96d
Pavel Ershov <redclover.p@gmail.com>
2026-01-18 11:27:19 +0300

    Написана логика ходов и отображения результата

--- script.js
+++ script.js
@@ -10,35 +10,60 @@ const hubImg = {
   // привязка переменных к классу
 let viewFirstCube = document.querySelector('.firstCube'),
     viewSecondCube = document.querySelector('.secondCube'),
+    viewFirstCubeBot = document.querySelector('.firstCubeBot'),
+    viewSecondCubeBot = document.querySelector('.secondCubeBot'),
     move = document.querySelector('#move'),
     countMove = document.querySelector('#countMove'),
     heartsPlayer = document.querySelector('.heartsPlayer'),
     heartsBot = document.querySelector('.heartsBot'),
     btn = document.querySelector('button')
   // инициализация переменных для привязки к резульату рандома
-let firstCube, secondCube;
+let firstCube, secondCube, playerCost, botCost;
   
 
   // запуск
 function start() {
-  btn.setAttribute('disabled', 'disabled')
-    // проверка и удаление img
+  btn.setAttribute('disabled', 'disabled');
+  movePlayer();
+  move.textContent = "Ход опоннента";
+  countMove.textContent = `Ваш счет: ${playerCost} - Бот: `;
+  (setTimeout(() => {
+    moveBot();
+    countMove.textContent = `Ваш счет: ${playerCost} - Бот: ${botCost}`;
+    winner();
+     btn.removeAttribute('disabled');
+     move.textContent = "Ваш ход";
+  }, 3000))
+
+  
+}
+
+function movePlayer() {
   if (viewFirstCube.children.length > 0 && viewSecondCube.children.length > 0) {
     viewFirstCube.children[0].remove();
     viewSecondCube.children[0].remove();
   };
-  //запуск генерации рандома
   random(1, 6);
-  //вывод кубиков
   viewFirstCube.innerHTML = (hubImg[firstCube]);
   viewSecondCube.innerHTML = (hubImg[secondCube]);
+  playerCost = firstCube + secondCube;
+  return playerCost;
+}
 
+function moveBot() {
+  if (viewFirstCubeBot.children.length > 0 && viewSecondCubeBot.children.length > 0) {
+    viewFirstCubeBot.children[0].remove();
+    viewSecondCubeBot.children[0].remove();
+  };
+  random(1, 6);
+  botCost = firstCube + secondCube;
+  viewFirstCubeBot.innerHTML = (hubImg[firstCube]);
+  viewSecondCubeBot.innerHTML = (hubImg[secondCube]);
+  return botCost;
+}
 
-
+function winner(player, bot) {
   
-  (setTimeout(() => {
-    btn.removeAttribute('disabled')
-  },3500))
 }
   // Генерация рандома
 function random(min, max) {
